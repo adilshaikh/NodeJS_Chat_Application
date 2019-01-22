9f2e9a2f99dfc1f40f64beb5ba2ebd66853f52f7
Adil Shaikh <adilayan94@gmail.com>
2019-01-22 12:07:11 +0000

    CLient -Server Socket connection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 const path = require('path');
 const express = require('express');
-const port = process.env.PORT || 3000
-var publicPath = path.join(__dirname, '/../public')
+const http = require('http');
+const socketIO = require('socket.io');
+const port = process.env.PORT || 3000;
+
+var publicPath = path.join(__dirname, '../public')
 
 // console.log(__dirname + './../public');
 // console.log(publicPath)
 var app = express();
+var server = http.createServer(app)
+var io = socketIO(server)
+
 app.use(express.static(publicPath))
 
+io.on('connection', (socket)=>{    //.on('event', callbackFunction)
+    console.log('New User Connected')
 
-app.listen(port, ()=>{
+    socket.on('disconnect', ()=>{
+        console.log('Client Disconnected.')
+    })
+})
+
+server.listen(port, ()=>{
     console.log(`Server is hosted on port ${port}`)
 })
